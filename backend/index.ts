8de539dbd611fa18f7f19a0648164de2bb3e2a60
Alex López <coquixo98@gmail.com>
2024-04-28 19:05:44 +0200

    add cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,17 @@
 import express from "express";
+import cors from "cors";
 const app = express();
+
 
 require("dotenv").config();
 
 const PORT = process.env.APP_PORT;
-
+app.use(cors());
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.json({ data: "Hello from Express!"});
 })
