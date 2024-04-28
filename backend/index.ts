578507eeb03ad3e7ee9f1cda36e6fd50d6198e23
Alex López <coquixo98@gmail.com>
2024-04-28 17:21:37 +0200

    server configured to open on port 8000

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,12 @@ const app = express();
 
 require("dotenv").config();
 
-app.listen(process.env.APP_PORT, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.APP_PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
